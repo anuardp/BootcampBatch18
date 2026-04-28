79df2a428f4aa707ae89a32af6f1fe8f9cd53c51
anuardp <nuradibphasya@gmail.com>
2026-04-28 09:56:00 +0700

    add: unit testing folders of testing game logic

---
+++ Projects/Poker/Game.Tests/Game_DrawCard_IsCardDrawFromTop.cs
@@ -0,0 +1,16 @@
+using Game;
+namespace Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+    [Test]
+    public void ()
+    {
+        Assert.Pass();
+    }
+}
