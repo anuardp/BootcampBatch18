0a60ccf86baa23b98edc27663c07686441361b06
Aulia Nur Adib Phasya <nuradibphasya@gmail.com>
2026-05-08 06:43:45 +0700

    test fibonacci

---
+++ Self-Experiments/FibonacciTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+int Fibonacci(int n)
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+// int number = 45;
+// Console.WriteLine(Fibonacci(number));
+
+
+
+
+void TestFibPerformance()
+{
+    Stopwatch sw = Stopwatch.StartNew();
+    int result = Fibonacci(35);
+    sw.Stop();
+    Console.WriteLine($"Fibonacci(35) = {result}, time = {sw.ElapsedMilliseconds} ms");
+    if (sw.ElapsedMilliseconds > 1000)
+        Console.WriteLine("Too Slow...");
+}
+TestFibPerformance();
